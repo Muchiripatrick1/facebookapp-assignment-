5ca8f0874bedf4ee3b7869cc619a09e66f127c6b
esthergithub <esther.mugo@africascan.com>
2021-06-04 17:08:29 +0300

    friends-connection-application-correction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,15 +2,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to user_path(@user.id)
-    else
-      render 'new'
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to user_path(@user.id)
+      else
+        render :new
+      end
     end
-  end
 
   def show
     @user = User.find(params[:id])
